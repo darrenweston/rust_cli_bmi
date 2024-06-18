020cac3897ea438d3aa50a205495709f19837266
Darren Weston <daz.weston@gmail.com>
2024-06-18 10:28:33 +0100

    format code

--- src/main.rs
+++ src/main.rs
@@ -39,9 +39,8 @@ fn get_input() -> Result<Data, String> {
 
     if height <= 0.0 {
         Err("Height must be greater than 0!".to_string())
-    }
-    else{
-        Ok(Data {weight, height})
+    } else {
+        Ok(Data { weight, height })
     }
 }
 
@@ -63,8 +62,9 @@ fn show_bmi(bmi: f32) {
 fn show_vertical_bmi(bmi: f32) {
     const REPS: usize = 4;
     let spacer = "|\n".repeat(REPS).trim_end().to_string();
-    let spacer2 = "|\n".repeat(REPS-2).trim_end().to_string();
-    let txt = format!("\
+    let spacer2 = "|\n".repeat(REPS - 2).trim_end().to_string();
+    let txt = format!(
+        "\
 -underweight
 {spacer2}
 |-healthy (18.5)
@@ -77,27 +77,25 @@ fn show_vertical_bmi(bmi: f32) {
 {spacer}
 |-obese (35.0)
 {spacer}
-");
+"
+    );
 
-    let pos = (((REPS+1) as f32)*5.0 * (bmi-15.)/(40.-15.)) as usize;
+    let pos = (((REPS + 1) as f32) * 5.0 * (bmi - 15.) / (40. - 15.)) as usize;
     let you = format!("You ({bmi:04.1}) -> |-");
 
     let mapper = |s| format!("              |-{s}");
 
-    let pre: String = txt.split('|')
-        .take(pos).map(mapper).collect();
-    let mid: String= txt.split('|')
-        .skip(pos).take(1).collect();
-    let post: String = txt.split('|')
-        .skip(pos+1).map(mapper).collect();
+    let pre: String = txt.split('|').take(pos).map(mapper).collect();
+    let mid: String = txt.split('|').skip(pos).take(1).collect();
+    let post: String = txt.split('|').skip(pos + 1).map(mapper).collect();
 
     let txt = format!("{pre}{you}{mid}{post}");
 
     //println!("{}", txt.split('\n').rev().map(|s| format!("{s}\n")).collect::<String>());
-    let output =
-        txt.split('\n')
-            .rev()
-            .fold(String::new(), |acc, s| acc + s + "\n");
+    let output = txt
+        .split('\n')
+        .rev()
+        .fold(String::new(), |acc, s| acc + s + "\n");
     println!("{output}");
 }
 
